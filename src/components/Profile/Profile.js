4caf9b091a25ad77c278b4381ebfc5730ce74b5a
Telgou <gamgamiahmed@yahoo.fr>
2024-04-14 16:35:12 +0100

    .

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -58,7 +58,7 @@ function Profile({language}) {
           <CustomTimelineItem title={language =="en" ?"Phone":"Tél"} text={resumeDataTranslated.phone} />
         </CustomTimeline>
         <div className={"button_container"}>
-          <a href="https://drive.google.com/uc?export=download&id=1wsiZI22TmYRSD_yWs2NN1p7UqLOeQrgrx" download={true}>
+          <a href="https://drive.google.com/uc?export=download&id=1wsiZI22TmYRSD_yWs2NN1p7UqLOeQrgr" download={true}>
             <CustomButton text={language =="en" ? "Download CV" : "Télécharger CV"} icon={<GetAppIcon />} />
           </a>
         </div>
